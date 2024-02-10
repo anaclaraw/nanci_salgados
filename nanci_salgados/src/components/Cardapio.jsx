4bf9d17ce82df6528215f8456d551d103228aed2
Ana Clara <anaclaramelo2707@gmail.com>
2024-02-10 00:35:58 -0300

    style: add hover effects

--- nanci_salgados/src/components/Cardapio.jsx
+++ nanci_salgados/src/components/Cardapio.jsx
@@ -26,8 +26,8 @@ function Card({salgados}) {
   console.log(salgados);
   return(
   salgados.map((salgado)=>(
-    <div  key={salgado.id} className='bg-white opacity-85 m-3 md:m-8 rounded-t-full rounded-b-[5000px] shadow-sm pb-2 flex flex-col max-w-[30vw] md:max-w-[20vw] items-center justify-center '>
-    <img  src={salgado.img} className='w-full rounded-full shadow-lg' alt="imagem_salgado" width="100%" />
+    <div  key={salgado.id} className='bg-white opacity-90 hover:opacity-85 hover:scale-[1.02]  transition-all  m-3 md:m-8 rounded-t-full rounded-b-[5000px] shadow-sm pb-2 flex flex-col max-w-[30vw] md:max-w-[20vw] items-center justify-center '>
+    <img  src={salgado.img} className='w-full rounded-full shadow-lg ' alt="imagem_salgado" width="100%" />
     <p className='font-semibold font-jet for md:text-2xl pt-1'>{salgado.title}</p>
     <p className='opacity-75 text-[10px] md:text-xl px-2'>{salgado.sabores}</p>   
     </div>
@@ -41,9 +41,9 @@ function Prices(){
   return(
     <div className='bg-white opacity-95 justify-center rounded-3xl shadow-md flex flex-col items-center w-[90%]  m-auto mt-12 md:mt-20 md:w-full md:rounded-none'>
       <div className='grid grid-cols-3 gap-4 md:gap-10 -mt-9'>
-        <div className='bg-black    font-jet  text-white rounded-full w-min p-1  md:p-8 flex items-center justify-center flex-col md:text-2xl '>30<p > salgados</p><h4 className='text-yellow-500 md:text-2xl font-bold'>R$50</h4> </div>
-        <div className='bg-red-700  font-jet  text-white rounded-full w-min  p-1 md:p-8  flex items-center justify-center flex-col md:text-2xl'> 50<p > salgados</p><h4 className='text-yellow-500 md:text-2xl font-bold '>R$70</h4></div>
-        <div className='bg-white    font-jet  text-black rounded-full w-min  p-1 md:p-8 border border-orange-950 flex items-center justify-center flex-col md:text-2xl '>100<p > salgados</p> <h4 className='text-yellow-500 md:text-2xl font-bold'>R$100</h4> </div>
+        <div className='bg-black    font-jet  text-white rounded-full w-min p-1  md:p-8 flex items-center justify-center flex-col md:text-2xl hover:scale-105 transition-all '>30<p > salgados</p><h4 className='text-yellow-500 md:text-2xl font-bold'>R$50</h4> </div>
+        <div className='bg-red-700  font-jet  text-white rounded-full w-min  p-1 md:p-8  flex items-center justify-center flex-col md:text-2xl hover:scale-105 transition-all'> 50<p > salgados</p><h4 className='text-yellow-500 md:text-2xl font-bold '>R$70</h4></div>
+        <div className='bg-white    font-jet  text-black rounded-full w-min  p-1 md:p-8 border border-orange-950 flex items-center justify-center flex-col md:text-2xl hover:scale-105 transition-all '>100<p > salgados</p> <h4 className='text-yellow-500 md:text-2xl font-bold'>R$100</h4> </div>
      
       </div>
       <p><strong>Ou monte seu kit</strong></p>
--- nanci_salgados/src/components/Socials.jsx
+++ nanci_salgados/src/components/Socials.jsx
@@ -10,13 +10,13 @@ const Socials = () => {
     <div ref={visible} className={` mt-32 lg:mt-44 transition-opacity ease-in duration-700 ${isVisible? "opacity-100" : "opacity-0"}`} id='contato'>
         <TitleModel title="NOSSAS REDES" subtitle="CONTATO"/>
         <section role='redes-sociais' className='bg-gray-200 w-full mt-6 flex  justify-around h-full items-center' >
-            <img src='/assets/img/post.png' className='w-[30vw] h-[60vw] -mt-4 -mb-4 m-auto rounded-lg shadow-lg shadow-gray-400' ></img>
+            <img src='/assets/img/post.png' className='w-[30vw] h-[60vw]  -mt-4 -mb-4 m-auto rounded-lg shadow-lg shadow-gray-400' ></img>
             
             <div className='w-1/2 flex flex-col items-center lg:gap-8 gap-3 h-full'>
-                <img src='/assets/img/stars.png' className='w-1/2 md:mb-5'/>
-                <a className='bg-gray-300 w-4/5 md:w-2/3 md:p-3 rounded-3xl pr-1 flex items-center justify-evenly md:text-3xl ' href="https://www.instagram.com/nanci_salgados1/">  <FaInstagram className='text-white md:text-6xl text-xl p-1 md:p-2 bg-pink-600 -mt-8 -mb-8 rounded-full'/> <p>@nanci_salgados</p> </a>
-                <a className='bg-gray-300 w-4/5 md:w-2/3 md:p-3 rounded-3xl pr-1 flex items-center justify-evenly md:text-3xl ' href="https://wa.me/">  <FaWhatsapp  className='text-white md:text-6xl text-xl p-1 md:p-2 bg-green-600 -mt-8 -mb-8 rounded-full'/> <p>(11)99999-9999</p> </a>
-                <a className='bg-gray-300 w-4/5 md:w-2/3 md:p-3 rounded-3xl pr-1 flex items-center justify-evenly md:text-3xl ' href="https://www.facebook.com/">  <FaFacebook className='text-white md:text-6xl text-xl p-1 md:p-2 bg-blue-600 -mt-8 -mb-8 rounded-full'/> <p>@nanci_salgados</p> </a>
+                <img src='/assets/img/stars.png' className='w-1/2 md:mb-5 animate-bounce'/>
+                <a className='bg-gray-300 w-4/5 md:w-2/3 md:p-3 rounded-3xl pr-1 flex items-center justify-evenly md:text-3xl hover:bg-pink-100' href="https://www.instagram.com/nanci_salgados1/">  <FaInstagram className='text-white md:text-6xl text-xl p-1 md:p-2 bg-pink-600 -mt-8 -mb-8 rounded-full'/> <p>@nanci_salgados</p> </a>
+                <a className='bg-gray-300 w-4/5 md:w-2/3 md:p-3 rounded-3xl pr-1 flex items-center justify-evenly md:text-3xl hover:bg-green-100' href="https://wa.me/">  <FaWhatsapp  className='text-white md:text-6xl text-xl p-1 md:p-2 bg-green-600 -mt-8 -mb-8 rounded-full'/> <p>(11)99999-9999</p> </a>
+                <a className='bg-gray-300 w-4/5 md:w-2/3 md:p-3 rounded-3xl pr-1 flex items-center justify-evenly md:text-3xl hover:bg-blue-200' href="https://www.facebook.com/">  <FaFacebook className='text-white md:text-6xl text-xl p-1 md:p-2 bg-blue-600 -mt-8 -mb-8 rounded-full'/> <p>@nanci_salgados</p> </a>
             </div>
             
         </section>
